120a30455dcbe6d7273b50256a5be72fbda766f6
cbartram <cbartram@gatech.edu>
2024-11-02 11:19:19 -0400

    feat(init): Add build.gradle

--- src/main/java/com/kraken/KrakenLoaderConfig.java
+++ src/main/java/com/kraken/KrakenLoaderConfig.java
@@ -2,6 +2,7 @@ package com.kraken;
 
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
+import net.runelite.client.config.ConfigItem;
 import net.runelite.client.config.ConfigSection;
 
 @ConfigGroup("krakenloaderplugin")
@@ -12,4 +13,17 @@ public interface KrakenLoaderConfig extends Config {
             position = 0
     )
     String general = "General";
+
+
+    // General
+    @ConfigItem(
+            keyName = "hydraImmunityOutline",
+            name = "Hydra immunity outline",
+            description = "Overlay the hydra with a colored outline while it has immunity/not weakened.",
+            position = 0,
+            section = general
+    )
+    default boolean hydraImmunityOutline() {
+        return false;
+    }
 }
--- src/test/java/com/kraken/KrakenLoaderPluginTest.java
+++ src/test/java/com/kraken/KrakenLoaderPluginTest.java
@@ -5,7 +5,6 @@ import net.runelite.client.externalplugins.ExternalPluginManager;
 
 public class KrakenLoaderPluginTest {
     public static void main(String[] args) throws Exception {
-        System.out.println("External plugin manager loading?");
         ExternalPluginManager.loadBuiltin(KrakenLoaderPlugin.class);
         RuneLite.main(args);
     }
